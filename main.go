891823a7261949a34dd83648f7447bb30f1b4e96
Kelsey Hightower <kelsey.hightower@gmail.com>
2018-04-06 19:37:12 +0000

    changing stuff

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	log.Println("Starting helloworld application...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world!\n")
+		fmt.Fprintf(w, "Hello universe!\n")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
